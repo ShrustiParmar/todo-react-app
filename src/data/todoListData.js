b18c9cbff41354cf97a0ff3af6effa8cd8ff0109
Shrusti <shrustiparmar1411@gmail.com>
2022-02-24 21:15:25 +0530

    To-do list application in React - part 1: UI and component structure

---
+++ src/data/todoListData.js
@@ -0,0 +1,17 @@
+const todoListData1 = [
+  { status: true, text: "Buy new sweatshirt" },
+  { status: true, text: "Begin promotional phase" },
+  { status: true, text: "Read an article" },
+  { status: false, text: "Try not to fall asleep" },
+  { status: false, text: "Watch 'Sherlock'" },
+  { status: false, text: "Begin QA for product" },
+  { status: false, text: "Go for a walk" },
+];
+
+const todoListData2 = [];
+
+function getData() {
+  return todoListData1;
+}
+
+export default getData;
